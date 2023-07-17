3b9b8e95f8f79da3674fca30935273b89c1d7efa
Liluter <karolawdziewicz@gmail.com>
2023-07-17 02:23:59 +0200

    SAD-13 add logic ...

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -69,20 +69,80 @@ class Input {
   }
 }
 
+class SwitchInd extends Component {
+  constructor(hookId, classes, attr) {
+    super(hookId, false);
+    this.classes = classes;
+    this.render();
+  }
+
+  switchHandler(e) {
+    e.target.classList.toggle("off");
+  }
+
+  render() {
+    this.elem = this.createComp("button", this.classes, this.attr);
+    this.elem.addEventListener("click", this.switchHandler);
+  }
+}
+
+class SliderMenuClosed extends Component {
+  list = ["mod", "switch", "delete"];
+  constructor(hookId, attr) {
+    super(hookId, false);
+    this.attr = attr;
+    this.elem;
+    this.render();
+  }
+
+  render() {
+    const sliderClo = this.createComp("div", "slider-menu-closed", this.attr);
+    for (let el of this.list) {
+      console.log(el)
+      const newIt = new SwitchInd(sliderClo.id, el);
+      sliderClo.append(newIt.elem);
+    }
+  }
+}
+
+
 class OperationBtnInput extends Component {
   constructor(hookId, operator, attr) {
     super(hookId, false);
     this.operator = operator;
     this.attr = attr;
     this.render();
+    this.elem;
   }
 
-  operationHandler() {}
+  // operationHandler(e) {
+  //   console.log(e.target)
+  // }
 
   render() {
-    const operBtn = this.createComp("button", "operation-btn");
-    operBtn.innerHTML = this.operator;
-    operBtn.addEventListener("click", this.operationHandler);
+    this.elem = this.createComp("button", "operation-btn");
+    this.elem.innerHTML = this.operator;
+    // this.elem.addEventListener("click", this.operationHandler.bind(this));
+  }
+}
+
+class InputNumber extends Component {
+  constructor(hookId) {
+    super(hookId, false);
+    this.elem;
+    this.render();
+  }
+
+  valueHandler(e) {
+    Generator.inputs.find((e) => e.id === this.hookId).mainVal = e.target.value;
+    console.log("target val: ", e.target.value, "Items: ", Generator.inputs);
+  }
+
+  render() {
+    this.elem = this.createComp("input", "input-num", [
+      { name: "placeholder", value: "put number" },
+    ]);
+    this.elem.addEventListener("change", this.valueHandler.bind(this));
   }
 }
 
@@ -93,81 +153,77 @@ class OperatorModal extends Component {
   constructor(hookId, attr) {
     super(hookId, false);
     this.attr = attr;
-    this.elem
+    this.elem;
     this.render();
   }
   operatorHandler(e) {
-    e.target.parentElement.classList.toggle('hide');
+    e.target.parentElement.classList.toggle("hide");
+    Generator.inputs.find((e) => e.id === this.hookId).mainOperator =
+      e.target.innerHTML;
+    e.target.parentElement.parentElement.firstChild.innerHTML =
+      e.target.innerHTML;
+    console.log("Input list : ", Generator.inputs);
   }
 
   render() {
     this.elem = this.createComp("div", "operator-modal hide", this.attr);
-    console.log("Id oper mod:", this.elem.id);
-    // this.elem.addEventListener("click", this.operatorHandler, false);
 
     for (const btn of this.operators) {
-      console.log(btn);
-      console.log(this.operators);
-      let oprBtnMod = new OperationBtnInput(this.elem.id,btn);
-      btn.addEventListener('click', this.operatorHandler);
-      this.buttons.push(oprBtnMod);
-      // inModOprBtn.addEventListener('click', this.operatorHandler)
+      this.buttons.push(new OperationBtnInput(this.elem.id, btn));
     }
+    this.buttons.forEach((e) =>
+      e.elem.addEventListener("click", this.operatorHandler.bind(this))
+    );
   }
 }
 
-class InputMain extends Component {
-  constructor(hookId, attr) {
+class InputItem extends Component {
+  constructor(hookId, classes, attr) {
     super(hookId, false);
+    this.classes = classes;
     this.attr = attr;
     this.render();
     this.elem;
   }
 
   render() {
-    this.elem = this.createComp("div", "input-main", this.attr);
-    console.log("Test", this.elem.id);
+    this.elem = this.createComp("div", this.classes, this.attr);
   }
 }
 
-class InputContainer extends Component {
+class Generator {
   static inputs = [];
-
-  constructor(hookId) {
-    super(hookId, false);
-    this.render();
-  }
+  static oprMainBtn;
 
   static addInput() {
     const id = this.inputs.length + 1;
-    console.log("id ;", id);
+    const smallContId = "input-cont-" + id;
     const mainInputId = "InputMain-" + id;
     this.inputs.push(new Input(mainInputId));
-    const newInputMain = new InputMain("input-cont", [
+
+    new InputItem("container", "input-container", [
+      { name: "id", value: smallContId },
+    ]);
+    new InputItem(smallContId, "input-main", [
       { name: "id", value: mainInputId },
     ]);
-    console.log(newInputMain.elem);
-    const operBtn = new OperationBtnInput(mainInputId, "+");
+    new SliderMenuClosed(smallContId);
+    //Slider mod open
+    //Slider Switch open
+    //Slider delete open
+
+    //InputMain children
+    this.oprMainBtn = new OperationBtnInput(mainInputId, "+");
+    this.oprMainBtn.elem.addEventListener("click", (e) => {
+      e.target.nextSibling.classList.toggle("hide");
+    });
     const operModId = "OperMod-" + id;
-    const operMOdal = new OperatorModal(mainInputId, [
-      { name: "id", value: operModId },
-    ]);
-  }
-
-  static renderItems() {
-    const parent = document.getElementById("input-cont");
-  }
-
-  static clearStock() {
-    // this.inputs = [];
-  }
-  render() {
-    const inputIt = this.createComp("div", "input-container");
-    inputIt.id = "input-cont";
+    new OperatorModal(mainInputId, [{ name: "id", value: operModId }]);
+    new InputNumber(mainInputId);
   }
 }
 
-class OperationBtn extends Component {
+class NewInput extends Component {
   constructor(hookId, operator) {
     super(hookId, false);
     this.operator = operator;
@@ -175,19 +231,35 @@ class OperationBtn extends Component {
   }
 
   createNewInput() {
-    console.log("After + pressed new inputMain will create");
-    InputContainer.addInput();
-    InputContainer.renderItems();
+    Generator.addInput();
   }
 
   render() {
-    console.log("Render func in OperationBtnClass");
     const startOpr = this.createComp("button", "operation-btn bigger");
     startOpr.innerHTML = this.operator;
     startOpr.addEventListener("click", this.createNewInput.bind(this));
   }
 }
 
-const header = new HeaderComp("app");
-const startBtn = new OperationBtn("app", "+");
-const inputCon = new InputContainer("app");
+class Container extends Component {
+  constructor(hookId, ownId) {
+    super(hookId, false);
+    this.ownId = ownId;
+    this.render();
+  }
+
+  render() {
+    this.createComp("div", "container", [{ name: "id", value: this.ownId }]);
+  }
+}
+
+class App {
+  static init() {
+    const header = new HeaderComp("app");
+    const container = new Container("app", "container");
+    const generator = new Generator();
+    const startBtn = new NewInput("app", "+");
+  }
+}
+
+App.init();
