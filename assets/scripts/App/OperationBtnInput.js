6695abb4d3f2428bb69bdbfb9536074ad6215c46
Karol <karolawdziewicz@gmail.com>
2024-01-22 19:02:27 +0100

    SAD-32 refactor OperationBtnInput remove unused props.

--- assets/scripts/App/OperationBtnInput.js
+++ assets/scripts/App/OperationBtnInput.js
@@ -1,17 +1,15 @@
 import { Component } from "./Component.js";
 
 export class OperationBtnInput extends Component {
-	constructor(hookId, operator, classes = "", attr) {
+	constructor(hookId, operator) {
 		super(hookId, false);
 		this.operator = operator;
-		this.classes = classes;
-		this.attr = attr;
-		this.render();
 		this.elem;
+		this.render();
 	}
 
 	render() {
-		this.elem = this.createComp("button", "operation-btn " + this.classes);
+		this.elem = this.createComp("button", "operation-btn ");
 		this.elem.innerHTML = this.operator;
 		this.elem.addEventListener("click", (e) => {
 			e.target.nextSibling.classList.toggle("hide");
